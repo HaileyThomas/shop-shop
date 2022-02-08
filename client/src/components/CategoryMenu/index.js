fd8235623185b24bf0bb058b2628c26762cb72b7
Hailey Thomas <haileyraebeauty@gmail.com>
2022-02-08 16:00:28 -0700

    check code and fix

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,15 +1,17 @@
 import React, { useEffect } from "react";
+import { useQuery } from "@apollo/client";
+import { useStoreContext } from "../../utils/GlobalState";
 import {
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
 } from "../../utils/actions";
-import { useQuery } from "@apollo/client";
 import { QUERY_CATEGORIES } from "../../utils/queries";
-import { useStoreContext } from "../../utils/GlobalState";
 
 function CategoryMenu() {
   const [state, dispatch] = useStoreContext();
+
   const { categories } = state;
+
   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
 
   useEffect(() => {
--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,14 +1,13 @@
 import React, { useEffect } from "react";
 import { useQuery } from "@apollo/client";
 
+import ProductItem from "../ProductItem";
 import { useStoreContext } from "../../utils/GlobalState";
 import { UPDATE_PRODUCTS } from "../../utils/actions";
-
-import ProductItem from "../ProductItem";
 import { QUERY_PRODUCTS } from "../../utils/queries";
 import spinner from "../../assets/spinner.gif";
 
-function ProductList({ currentCategory }) {
+function ProductList() {
   const [state, dispatch] = useStoreContext();
 
   const { currentCategory } = state;
@@ -30,14 +29,14 @@ function ProductList({ currentCategory }) {
     }
 
     return state.products.filter(
-      (product) => PerformanceResourceTiming.category._id === currentCategory
+      (product) => product.category._id === currentCategory
     );
   }
 
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
-      {products.length ? (
+      {state.products.length ? (
         <div className="flex-row">
           {filterProducts().map((product) => (
             <ProductItem
