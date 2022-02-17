53c18e8d6d5afd83799e96bc79b0c9ed271ca688
Hailey Thomas <haileyraebeauty@gmail.com>
2022-02-17 11:18:11 -0700

    create IDB helper function

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,6 +1,59 @@
 export function pluralize(name, count) {
   if (count === 1) {
-    return name
+    return name;
   }
-  return name + 's'
+  return name + "s";
+}
+
+export function idbPromise(storeName, method, object) {
+  return new Promise((resolve, reject) => {
+    // open connection to the database `shop-shop` with version of 1
+    const request = window.indexedDB.open("shop-shop", 1);
+
+    // create variables to hold reference to the database, transaction and the object store
+    let db, tex, store;
+
+    // if version has changed or first time using database, run and create the 3 objects stores
+    request.onupgradeneeded = function (e) {
+      const db = request.result;
+      db.createObjectStore("products", { keyPath: "_id" });
+      db.createObjectStore("categories", { keyPath: "_id" });
+      db.createObjectStore("cart", { keyPath: "_id" });
+    };
+
+    // handle any errors with connecting
+    request.onerror = function (e) {
+      console.log("There was an error");
+    };
+
+    // on database open success
+    request.onsuccess = function (e) {
+      console.log("error", e);
+    };
+
+    // check which value is passed into the function as a method and perform that method on the object store
+    switch (method) {
+      case "put":
+        store.put(object);
+        resolve(object);
+        break;
+      case "get":
+        const all = store.getAll();
+        all.onsuccess = function () {
+          resolve(all.result);
+        };
+        break;
+      case "delete":
+        store.delete(object._id);
+        break;
+      default:
+        console.log("No valid method");
+        break;
+    }
+
+    // when the transaction is complete, close the connection
+    tx.oncomplete = function () {
+      db.close();
+    };
+  });
 }
