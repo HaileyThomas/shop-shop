2d99d5b7a4f0f3f81f035f04d49ea78b84da13b8
Hailey Thomas <haileyraebeauty@gmail.com>
2022-02-18 14:49:30 -0700

    finish implementing IndexedDB

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -7,13 +7,8 @@ export function pluralize(name, count) {
 
 export function idbPromise(storeName, method, object) {
   return new Promise((resolve, reject) => {
-    // open connection to the database `shop-shop` with version of 1
     const request = window.indexedDB.open("shop-shop", 1);
-
-    // create variables to hold reference to the database, transaction and the object store
-    let db, tex, store;
-
-    // if version has changed or first time using database, run and create the 3 objects stores
+    let db, tx, store;
     request.onupgradeneeded = function (e) {
       const db = request.result;
       db.createObjectStore("products", { keyPath: "_id" });
@@ -21,39 +16,41 @@ export function idbPromise(storeName, method, object) {
       db.createObjectStore("cart", { keyPath: "_id" });
     };
 
-    // handle any errors with connecting
     request.onerror = function (e) {
       console.log("There was an error");
     };
 
-    // on database open success
     request.onsuccess = function (e) {
-      console.log("error", e);
-    };
+      db = request.result;
+      tx = db.transaction(storeName, "readwrite");
+      store = tx.objectStore(storeName);
 
-    // check which value is passed into the function as a method and perform that method on the object store
-    switch (method) {
-      case "put":
-        store.put(object);
-        resolve(object);
-        break;
-      case "get":
-        const all = store.getAll();
-        all.onsuccess = function () {
-          resolve(all.result);
-        };
-        break;
-      case "delete":
-        store.delete(object._id);
-        break;
-      default:
-        console.log("No valid method");
-        break;
-    }
+      db.onerror = function (e) {
+        console.log("error", e);
+      };
 
-    // when the transaction is complete, close the connection
-    tx.oncomplete = function () {
-      db.close();
+      switch (method) {
+        case "put":
+          store.put(object);
+          resolve(object);
+          break;
+        case "get":
+          const all = store.getAll();
+          all.onsuccess = function () {
+            resolve(all.result);
+          };
+          break;
+        case "delete":
+          store.delete(object._id);
+          break;
+        default:
+          console.log("No valid method");
+          break;
+      }
+
+      tx.oncomplete = function () {
+        db.close();
+      };
     };
   });
 }
